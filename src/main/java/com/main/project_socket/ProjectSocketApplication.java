2d57e715689cdb32482b8dfeada948fbeca6fb2e
P.Cardoso <www.pedrolucascardoso@gmail.com>
2025-04-21 21:19:11 -0300

    first commit

---
+++ src/main/java/com/main/project_socket/ProjectSocketApplication.java
@@ -0,0 +1,13 @@
+package com.main.project_socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/project_socket/ProjectSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.project_socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
