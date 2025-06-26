6172d15ff0e22293a166333e168375a6918335c8
Pedro Cardoso Duett Software <pedro.cardoso@duettsoftware.com>
2025-06-26 10:48:32 -0300

    Profile Body and Profile Entity

---
+++ src/main/java/com/main/project_socket/entity/dto/ProfileBody.java
@@ -0,0 +1,7 @@
+package com.main.project_socket.entity.dto;
+
+public record ProfileBody(String name, String email, String phone) {
+    // This record class is used to encapsulate the profile data
+    // It provides a concise way to define immutable data structures
+    // with named fields.
+}
--- src/main/java/com/main/project_socket/service/ProfileServices.java
+++ src/main/java/com/main/project_socket/service/ProfileServices.java
@@ -2,12 +2,19 @@ package com.main.project_socket.service;
 
 import com.main.project_socket.entity.Profile;
 import com.main.project_socket.interfaces.ProfileInterfaceService;
+import com.main.project_socket.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileServices implements ProfileInterfaceService {
+
+    @Autowired
+    private ProfileRepository repository;
+
     @Override
     public Profile createdProfile(String name, String email, String phone) {
+
         return null;
     }
 
