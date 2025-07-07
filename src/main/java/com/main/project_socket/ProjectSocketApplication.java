31e5d7a383855306b0a57b1fb9b01154c80ac7b2
P.Cardoso <www.pedrolucascardoso@gmail.com>
2025-07-06 22:33:26 -0300

    Update ProjectSocketApplication.java

--- src/main/java/com/main/project_socket/ProjectSocketApplication.java
+++ src/main/java/com/main/project_socket/ProjectSocketApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectSocketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectSocketApplication.class, args);
 		System.out.println("--------------------------------");
 		System.out.println("System of Web Message");
 		System.out.println("--------------------------------");
 	}
-
 }
